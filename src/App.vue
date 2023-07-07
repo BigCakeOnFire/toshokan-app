88ae9c5dc072ad2187bbee6acd248c4f228e8951
BigCakeOnFire <125418870+BigCakeOnFire@users.noreply.github.com>
2023-07-08 02:20:35 +0900

    create

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import SrchHeader from './components/SrchHeader.vue';
+import ShosekiTableMain from './components/ShosekiTableMain.vue';
+</script>
+<template>
+    <div class="wrap">
+        <div>ヘッダー</div>
+        <SrchHeader />
+        <ShosekiTableMain />
+        <main class="main">メイン</main>
+        <div>フッター</div>
+    </div>
+</template>
+
+<style>
+@import 'assets/css/destyle.css-3.0.2/';
+.wrap{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    min-height: 100vh;
+    width: 600px;
+    margin: 0 auto;
+    font-family: sans-serif;
+}
+
+.main{
+    flex: 1;
+    width: 100%;
+}
+</style>
---
+++ src/components/SrchHeader.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { ref } from 'vue';
+// 変数宣言
+const shosekiForm = ref('');
+const sakushaForm = ref('');
+
+// 検索イベント
+const srchEvent = () => {
+    if (shosekiForm.value != null && shosekiForm.value !== '') {
+        console.log(shosekiForm.value);
+    } else {
+        console.log('書籍空');
+    }
+    if (sakushaForm.value != null && sakushaForm.value !== '') {
+        console.log(sakushaForm.value);
+    } else {
+        console.log('作者空');
+    }
+
+}
+</script>
+
+<template>
+    <h1 class="title">書籍検索</h1>
+    <div class="srch">
+        <input type="text" class="form" v-model="shosekiForm" placeholder="+書籍名を入力" />
+        <input type="text" class="form" v-model="sakushaForm" placeholder="+作者名を入力" />
+        <button class="btn" @click="srchEvent">検索</button>
+    </div>
+    {{ shosekiForm }}
+    {{ sakushaForm }}
+</template>
+
+<style scoped>
+.title{
+    width: 100%;
+    background-color: #e3f2fd;
+    text-align: center;
+    font-size: 32px;
+    font-weight: bold;
+    padding: 8px 0;
+}
+
+.form{
+    background-color: green;
+}
+.srch{
+    float: left;
+}
+.btn{
+    padding: 8px;
+    background-color: rgb(59, 228, 59);
+    border-radius: 6px;
+    color: white;
+    text-align: center;
+    font-size: 14px;
+}
+</style>
